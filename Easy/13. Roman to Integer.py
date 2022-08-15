24653095e4147808979ec256d169c80ffc957682
Muhammad Anas <mrwhosetheboss365@gmail.com>
2022-08-15 21:19:17 +0500

    Completed LeetCode Problem 13 - Roman to Numerals
    Problem LeetCode 14 has been solved - **FINALLY**!

--- Easy/13. Roman to Integer.py
+++ Easy/13. Roman to Integer.py
@@ -39,11 +39,23 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        hashmap = {
+        roman_numerals = {
             "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000,
         }
-
-
+        output = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_numerals[s[i]] < roman_numerals[s[i + 1]]:
+                output -= roman_numerals[s[i]]
+            else:
+                output += roman_numerals[s[i]]
+        
+        return output
 
 sol = Solution()
-sol.romanToInt("III")
+print(sol.romanToInt("IV"))
