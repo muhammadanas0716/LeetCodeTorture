b6be61dd9c0eccd6d5978ec27d07267866dc196d
Sanskrati <31sanskrati@gmail.com>
2022-10-07 11:41:07 +0530

    Create 1773. Count Items Matching a Rule.cpp

---
+++ C++/Easy/1773. Count Items Matching a Rule.cpp
@@ -0,0 +1,51 @@
+//You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+//The ith item is said to match the rule if one of the following is true:
+//
+//ruleKey == "type" and ruleValue == typei.
+//ruleKey == "color" and ruleValue == colori.
+//ruleKey == "name" and ruleValue == namei.
+//Return the number of items that match the given rule.
+//
+//
+//Example 1:
+//
+//Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+//Output: 1
+//Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+//Example 2:
+//
+//Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+//Output: 2
+//Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+// 
+//
+//Constraints:
+//
+//1 <= items.length <= 104
+//1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+//ruleKey is equal to either "type", "color", or "name".
+//All strings consist only of lowercase letters.
+  
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int ans = 0;
+        int val;
+        if(ruleKey == "type"){
+            val = 0;
+        }
+        else if(ruleKey == "color"){
+            val = 1;
+        }
+        // ruleKey == "name"
+        else{
+            val = 2;
+        }
+        for(int i = 0; i<items.size(); i++){
+            if(items[i][val] == ruleValue){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
