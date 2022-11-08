4e7e1ea3b0ac7bc215c7b819b649dd0cfab459fa
Sanskrati <31sanskrati@gmail.com>
2022-11-09 02:21:06 +0530

    Leetcode problem solution in C++
    * Create 1295. Find Numbers with Even Number of Digits.cpp
    
    * Create 867. Transpose Matrix.cpp
    
    * Create 989. Add to Array-Form of Integer.cpp
    
    * Create 1854. Maximum Population Year.cpp
    
    * Create 1886. Determine Whether Matrix Can Be Obtained By Rotation.cpp
    
    * Create 1. Two Sum.cpp
    
    * Create 1304. Find N Unique Integers Sum up to Zero.cpp
    
    * Create 1380. Lucky Numbers in a Matrix.cpp
    
    * Create 53. Maximum Subarray.cpp
    
    * Create 566. Reshape the Matrix.cpp
    
    * Create 2437. Number of Valid Clock Times.cpp
    
    * Create 66. Plus One.cpp
    
    * Create 26. Remove Duplicates from Sorted Array.cpp
    
    * Create 1217. Minimum Cost to Move Chips to The Same Position.cpp
    
    * Create 54. Spiral Matrix.cpp
    
    * Create 59. Spiral Matrix II.cpp
    
    * Create 885. Spiral Matrix III.cpp
    
    * Delete 885. Spiral Matrix III.cpp
    
    * Create 73. Set Matrix Zeroes.cpp
    
    * Delete 73. Set Matrix Zeroes.cpp
    
    * Create 73. Set Matrix Zeroes.cpp
    
    * Create 885. Spiral Matrix III.cpp
    
    * Create 238. Product of Array Except Self.cpp
    
    * Create 34. Find First and Last Position of Element in Sorted Array.cpp
    
    * Create 189. Rotate Array.cpp
    
    * Create 55. Jump Game.cpp
    
    * Create 75. Sort Colors.cpp
    
    * Create 198. House Robber.cpp

---
+++ C++/Medium/75. Sort Colors.cpp
@@ -0,0 +1,45 @@
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+        
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[high], nums[mid]);
+                high--;
+            }
+        }
+    }
+};
