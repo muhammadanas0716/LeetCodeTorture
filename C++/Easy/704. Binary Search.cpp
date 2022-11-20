57cad4bf2772e287e74bca5298355a93c6f80a58
Sanskrati <31sanskrati@gmail.com>
2022-11-21 02:18:41 +0530

    Create 704. Binary Search.cpp

---
+++ C++/Easy/704. Binary Search.cpp
@@ -0,0 +1,45 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^4
+-10^4 < nums[i], target < 10^4
+All the integers in nums are unique.
+nums is sorted in ascending order.
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int h = nums.size() -1;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target)
+                l = mid + 1;
+            else
+                h = mid - 1;
+        }
+        return -1;
+    }
+};
